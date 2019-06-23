ceffae27f3966b391344e69d159477a9dc07e591
ZIN MYO <overmidnight12@gmail.com>
2019-06-23 10:31:56 -0700

    my changes

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html',{'name':'Thura'})
+
+def add(request):
+
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    res = val1 + val2
+
+    return render(request, 'result.html',{'result':res})
